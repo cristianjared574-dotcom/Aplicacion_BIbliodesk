1f8a3690567bfe4932de16433d61ad9af5c7291b
Carlos_Daniel_Alonso_Hernandez <fitnesshernandez026@gmail.com>
2026-07-17 13:04:48 -0600

    login,saquelo del empleado,listo pasa al inicio

--- Aplicacion_BIbliodesk/Conexion.cs
+++ Aplicacion_BIbliodesk/Conexion.cs
@@ -40,12 +40,5 @@ namespace Aplicacion_BIbliodesk
         }
     }
 
-    public class Empleado
-    {
-        public int IdEmpleado { get; set; }
-        public string NombreCompleto { get; set; }
-        public string Username { get; set; }
-        public string Rol { get; set; }
-        public string Estado { get; set; }
-    }
+  
 }
