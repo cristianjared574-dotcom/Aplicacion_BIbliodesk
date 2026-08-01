4c9329f47e3f1501c6d2eaf9273287e9e55a0fb1
Carlos_Daniel_Alonso_Hernandez <fitnesshernandez026@gmail.com>
2026-08-01 15:53:03 -0600

    boton de audio

---
+++ Aplicacion_BIbliodesk/sonido.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Aplicacion_BIbliodesk
+{
+    partial class sonido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnaudio1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnaudio1
+            // 
+            this.btnaudio1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnaudio1.Location = new System.Drawing.Point(0, -2);
+            this.btnaudio1.Name = "btnaudio1";
+            this.btnaudio1.Size = new System.Drawing.Size(190, 48);
+            this.btnaudio1.TabIndex = 0;
+            this.btnaudio1.Text = "Audio";
+            this.btnaudio1.UseVisualStyleBackColor = false;
+            this.btnaudio1.Click += new System.EventHandler(this.btnaudio1_Click);
+            // 
+            // sonido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 43);
+            this.Controls.Add(this.btnaudio1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "sonido";
+            this.ShowInTaskbar = false;
+            this.Text = "sonido";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnaudio1;
+    }
+}
---
+++ Aplicacion_BIbliodesk/sonido.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Aplicacion_BIbliodesk
+{
+    public partial class sonido : Form
+    {
+        private Form formPadre;
+
+        public sonido(Form origen)
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            ShowInTaskbar = false;
+            TopMost = true;
+            BackColor = Color.FromArgb(255, 228, 196);
+
+            formPadre = origen;
+        }
+
+        private void btnaudio1_Click(object sender, EventArgs e)
+        {
+            // Guardamos posición antes de abrir
+            Point posMenu = this.PointToScreen(Point.Empty);
+
+            //  el botón Audio se queda visible
+            // this.Close(); ELIMINADO
+
+            // opciones al lado izquierdo
+            audio ventanaOpciones = new audio(formPadre);
+            ventanaOpciones.ShowInTaskbar = false;
+            ventanaOpciones.TopMost = true;
+
+            ventanaOpciones.StartPosition = FormStartPosition.Manual;
+            ventanaOpciones.Location = new Point(posMenu.X - ventanaOpciones.Width, posMenu.Y);
+
+            // Solo al elegir opción se cierran las dos juntas
+            ventanaOpciones.FormClosed += (s, args) => this.Close();
+
+            ventanaOpciones.Show();
+            ventanaOpciones.BringToFront();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+        // protected override void OnDeactivate(EventArgs e)
+        // {
+        //     base.OnDeactivate(e);
+        //     this.Close();
+        // }
+    }
+}
